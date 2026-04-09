d40beadf3b40805e1c19215dcf1751313b560fb3
SantiagoGuty <santiago.gutierrezmorales@valpo.edu>
2026-04-09 11:12:38 -0400

    Sprint 1.3 config add ESLint flat config and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    ignores: ["node_modules/**", ".expo/**", "dist/**"],
+  },
+];
